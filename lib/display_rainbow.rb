733d6ce3842ac5f8ac8139b170c38833b46d07b1
Lynee Lawson <lyneelawson@gmail.com>
2021-02-23 14:40:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,4 @@ def display_rainbow (color_list)
 puts "R: #{color_list[0]}, O: #{color_list[1]}, Y: #{color_list[2]}, G: #{color_list[3]}, B: #{color_list[4]}, P: #{color_list[5]}, I: #{color_list[6]}, V: #{color_list[7]}"
 end
 
-display_rainbow(colors)
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
